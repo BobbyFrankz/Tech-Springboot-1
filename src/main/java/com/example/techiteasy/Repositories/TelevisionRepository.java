6610495dac9c9c482ff3b70309554b864ef5037e
BobbyFrankz <bobrapiets@gmail.com>
2022-11-22 11:34:46 +0100

    spring-homework

---
+++ src/main/java/com/example/techiteasy/Repositories/TelevisionRepository.java
@@ -0,0 +1,7 @@
+package com.example.techiteasy.Repositories;
+
+import com.example.techiteasy.Models.Television;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TelevisionRepository extends JpaRepository<Television, Long> {
+}
